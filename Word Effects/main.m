1748e1a8a4c48674c4ba42da27ad75ddd59e9595
Larry Luk <larryluk526@gmail.com>
2017-10-30 20:12:38 -0400

    final v2

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -11,48 +11,69 @@
 int main(int argc, const char * argv[]) {
   
     while (true) {
+        
         char inputChars[255];
         printf("Input a string: ");
         fgets(inputChars, 255, stdin);
+        if (inputChars[0] == '\n') {
+            fgets(inputChars, 255, stdin);
+        }
         printf("Your string is %s", inputChars);
         NSString *inputString = [NSString stringWithUTF8String:inputChars];
         NSLog(@"Input was : %@", inputString);
-        NSLog(@"1. Uppercase : %@", [inputString uppercaseString]);
-        NSLog(@"2. Lowercase : %@", [inputString lowercaseString]);
+        
+        int inputOps;
+        printf("Input an operation, your choices are 1. Uppercase, 2. Lowercase, 3. Numberize, 4. Canadianize, 5. Respond, 6. De-Space-it. ");
+        scanf("%d", &inputOps);
+        NSLog(@"Input was : %d", inputOps);
+
+
         
         int convertedValue = [inputString intValue];
-        if (convertedValue != 0) {
-            NSLog(@"3. Numberize : %d", [inputString intValue]);
-        } else {
-            NSLog(@"Cannot convert to integer, or the number input is 0");
-        }
-   
+        
         NSString *editedInput = [inputString stringByReplacingOccurrencesOfString:@"\n" withString:@""];
         NSString *ehInputString = @", eh?";
         NSString *combinedString = [editedInput stringByAppendingString:ehInputString];
-        NSLog(@"4. Canadianize %@", combinedString);
-        
-        if ([editedInput hasSuffix:@"!"]) {
-            NSLog(@"5. Respond Whoa calm down!");
-
-        } else if ([editedInput hasSuffix:@"?"]) {
-            NSLog(@"5. Respond I don't know");
-
-        } else {
-            NSLog(@"5. Respond : %@", inputString);
-        }
-        
-        
         
         NSString *noSpaceInput = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
-        NSLog(@"6. Despace %@", noSpaceInput);
 
 
-        
-
-
-
-        
+   
+        switch (inputOps) {
+            case 1:
+                NSLog(@"1. Uppercase : %@", [inputString uppercaseString]);
+                break;
+            case 2:
+                NSLog(@"2. Lowercase : %@", [inputString lowercaseString]);
+                break;
+            case 3:
+                if (convertedValue != 0) {
+                    NSLog(@"3. Numberize : %d", [inputString intValue]);
+                } else {
+                    NSLog(@"Cannot convert to integer, or the number input is 0");
+                }
+                break;
+            case 4:
+                NSLog(@"4. Canadianize %@", combinedString);
+                break;
+            case 5:
+                if ([editedInput hasSuffix:@"!"]) {
+                    NSLog(@"5. Respond Whoa calm down!");
+                    
+                } else if ([editedInput hasSuffix:@"?"]) {
+                    NSLog(@"5. Respond I don't know");
+                    
+                } else {
+                    NSLog(@"5. Respond : %@", inputString);
+                }
+                break;
+            case 6:
+                NSLog(@"6. Despace %@", noSpaceInput);
+                break;
+            default:
+                NSLog(@"Incorrect operation");
+                break;
+        }
 
     }
 
